c7b228782c268b4b008b9137e25e615140b8d7fc
Tim Wright <tenortim@gmail.com>
2018-01-06 21:48:13 -0800

    Checkpoint

--- statssink/common.go
+++ statssink/common.go
@@ -2,9 +2,10 @@ package statssink
 
 import "timw/isilon/gostats/papistats"
 
+// DBWriter defines an interface to write OneFS stats to a persistent store/database
 type DBWriter interface {
-  // Initialize a statssink
-  Init(a []string) error
-  // Write a stat to the sink
-  WriteStats(stats []papistats.StatResult) error
+	// Initialize a statssink
+	Init(a []string) error
+	// Write a stat to the sink
+	WriteStats(stats []papistats.StatResult) error
 }
